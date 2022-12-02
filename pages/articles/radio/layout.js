ab5f405b0fcb61bcf6bfef2918b8d4d080f4b46a
CMOISDEAD <camilodavina@gmail.com>
2022-12-02 12:39:02 -0500

    hotfix

--- pages/articles/radio/layout.js
+++ pages/articles/radio/layout.js
@@ -20,17 +20,19 @@ export default function LayoutRadio({
       <div className="overflow-hidden text-gray-700">
         <div className="container px-5 py-2 mx-auto lg:pt-12 lg:px-32">
           <div className="flex flex-wrap -m-1 md:-m-2">
-            {collage.map((img, i) => (
-              <div key={i} className="flex flex-wrap w-1/3">
-                <div className="w-full p-1 md:p-2">
-                  <img
-                    alt="gallery"
-                    className="block object-cover object-center w-full h-full rounded-lg"
-                    src={`/images/radio/${img}.jpg`}
-                  />
+            {collage
+              ? collage.map((img, i) => (
+                <div key={i} className="flex flex-wrap w-1/3">
+                  <div className="w-full p-1 md:p-2">
+                    <img
+                      alt="gallery"
+                      className="block object-cover object-center w-full h-full rounded-lg"
+                      src={`/images/radio/${img}.jpg`}
+                    />
+                  </div>
                 </div>
-              </div>
-            ))}
+              ))
+              : ""}
           </div>
           <p className="text-center italic text-white">{footer}</p>
           <p className="text-center italic text-white">
