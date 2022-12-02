9dee0d5901d6bf4cc7bf054227f272ad560376a5
CMOISDEAD <camilodavina@gmail.com>
2022-12-02 12:32:45 -0500

    hot fix

--- pages/articles/reportaje/index.js
+++ pages/articles/reportaje/index.js
@@ -4,7 +4,6 @@ const props = {
   title: "Repotaje",
   description:
     "En el siguiente reportaje se relata las causas y consecuencias que producen los recientes monocultivos de aguacate Hass en las montañas de la cordillera quindiana, los cuales vienen causando molestia en las poblaciones de estos municipios por todos los aspectos negativos que trae consigo estos cultivos de origen chileno, mexicano y colombiano y que de  manera masiva se han tomado grandes territorios montañosos provocando efectos perjudiciales en la tierra, vegetación, fauna y cuerpos fluviales.",
-  // image: "reportaje",
   collage: [
     "reportaje_1",
     "reportaje_2",
@@ -16,6 +15,7 @@ const props = {
   footer:
     "Nestor Ocampo, Edwin Reyes, Monica Florez, Manuela Gonzalez, Maria Luisa, Kevin Herrera",
   audio: "reportaje",
+  name: "",
 };
 
 export default function Cronica() {
